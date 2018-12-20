be9199baa51e9fe1a0d2c6604a2bdb9d52295446
lukesmith <me@lukesmith.net>
2018-12-20 22:42:08 +0000

    Support vscode on raspbian

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,3 +1,19 @@
-mkdir -p $HOME/.config/Code/User/
-rm "$HOME/.config/Code/User/settings.json"
-ln -s "$DOTFILES/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
+#!/usr/bin/env bash
+
+raspbian_code="$HOME/.config/Code - OSS (headmelted)"
+
+if [ -x "$(command -v raspi-config)" ]; then
+  if [ ! -d "$raspbian_code" ]; then
+    wget https://packagecloud.io/headmelted/codebuilds/gpgkey -O - | sudo apt-key add -
+    curl -L https://code.headmelted.com/installers/apt.sh | sudo bash
+  fi
+
+  if [ -d "$raspbian_code" ]; then
+    rm -rf "$raspbian_code/User/settings.json"
+    ln -s "$DOTFILES/vscode/settings.json" "$raspbian_code/User/settings.json"
+  fi
+else
+  mkdir -p $HOME/.config/Code/User/
+  rm -rf "$HOME/.config/Code/User/settings.json"
+  ln -s "$DOTFILES/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
+fi
